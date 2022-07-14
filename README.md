6eff845100db142b5e9f4a515a2598f31ffe7405
Adam K Gray <adamkgray@pm.me>
2022-07-14 12:52:01 +0100

    optimise

--- README.md
+++ README.md
@@ -6,22 +6,23 @@ It works like this:
 - the app prompts for a string and a fret. For example "2nd string, 7th fret"
 - you say the name of the note (and maybe play it too, but I avoid doing that because hearing the note can be a giveaway if you're good with pitch)
 
-The app is written to guarantee that all 6 strings and all 12 frets are prompted for at least once per cycle. That is to say, in 6 prompts you will have to play all 6 strings, and in 12 prompts you will have to use all 12 frets. It also uses randomises the voices so that it doesn't get _too_ repetitive.
+The app is written to guarantee that all 6 strings and all 12 frets are prompted for. There are 72 unique combinations of strings and frets ($6 \times 12$). So after 72 prompts you will have played every note. 
 
-It continues infinitely until you hit `ctrl-c`, whereupon it prints the number of exercises completed.
+It also uses randomises the voices so that it doesn't get _too_ repetitive. Voices have been selected for the clearest pronunciation, but you can edit the code to add any other voices you want.
 
+The app continues infinitely until you hit `ctrl-c`, whereupon it prints the number of exercises completed.
+
+The app is only intended to run on MacOS (using the `say` command).
 
 ## Usage
 
 ```shell
 $ python fretboard.py
-
-# custom delay of 12s between prompts
-$ python fretboard.py 12
 ```
 
 ## Config
 
-The app is only intended to run on MacOS (it uses the `say` command).
-
-Voices have been selected for the clearest pronunciation, but you can edit the code to add any other voices you want.
+```shell
+# custom delay of 12s between prompts
+$ python fretboard.py 12
+```
--- fretboard.py
+++ fretboard.py
@@ -1,3 +1,4 @@
+from itertools import product
 from sys import argv
 from random import shuffle
 from subprocess import check_call
@@ -10,18 +11,7 @@ try:
 except:
     pass
 
-strings = {
-    1: "first",
-    2: "second",
-    3: "third",
-    4: "fourth",
-    5: "fifth",
-    6: "sixth"
-}
-string_options = list(strings.keys())
-shuffle(string_options)
-
-frets = {
+ordinals = {
     1: "first",
     2: "second",
     3: "third",
@@ -35,8 +25,13 @@ frets = {
     11: "eleventh",
     12: "twelfth"
 }
-fret_options = list(frets.keys())
-shuffle(fret_options)
+
+prompts = list(product(
+    list(range(1, 7)),
+    list(range(1, 13))
+))
+shuffle(prompts)
+prompt_i = 0
 
 voices = [
     "Alex",
@@ -44,38 +39,28 @@ voices = [
     "Moira",
     "Samantha"
 ]
-
 shuffle(voices)
-
 voice_i = 0
-string_i = 0
-fret_i = 0
 
 completed = 0
 
 try:
     while True:
-        # select random string
-        string = strings[string_options[string_i]]
-
-        # select random fret
-        fret = frets[fret_options[fret_i]]
+        # select random string and fret
+        string_n, fret_n = prompts[prompt_i]
+        string = ordinals[string_n]
+        fret = ordinals[fret_n]
 
         # select random voice
         voice = voices[voice_i]
 
         # print prompt to console
-        print("{} string, {} fret ({})".format(
-            string_options[string_i],
-            fret_options[fret_i],
-            voice
-        ))
+        print("{} string, {} fret".format(string_n, fret_n))
 
         # say prompt
         check_call([
             "say",
-            "-v",
-            voice,
+            "-v", voice,
             "{} string, {} fret".format(string, fret)
         ])
 
@@ -85,17 +70,11 @@ try:
         sleep(PAUSE)
 
         # increment or reshuffle options
-        if string_i == len(strings) - 1:
-            string_i = 0
-            shuffle(string_options)
+        if prompt_i == len(prompts) - 1:
+            prompt_i = 0
+            shuffle(prompts)
         else:
-            string_i += 1
-
-        if fret_i == len(frets) - 1:
-            fret_i = 0
-            shuffle(fret_options)
-        else:
-            fret_i += 1
+            prompt_i += 1
 
         if voice_i == len(voices) - 1:
             voice_i = 0
